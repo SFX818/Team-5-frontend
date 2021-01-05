606267f5717297f91c5074ebe900ab2440bdba1b
nmuchiri <lonesomebounty@gmail.com>
2021-01-04 21:19:25 -0500

    trying to stash branch

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -5,12 +5,12 @@ const API_URL = "http://localhost:8080/"
 
 // function to save a new Event
 export const save = (eventId, name, date, location) => {
-    return axios.post(API_URL + 'profile/myevents/addevent', {
+    axios.post(API_URL + 'profile/myevents/addevent', {
         eventId,
         name,
         date,
         location
-    }, {header: authHeader()})
+    }, {header: authHeader()}).
 }
 
 
