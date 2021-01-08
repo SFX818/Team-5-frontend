67f1c637bcd3c6ade97e9aa30fcb63abb75551e4
“camillebrown” <camillebrown11@gmail.com>
2021-01-07 16:20:42 -0800

    updated back to route of myevent

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -39,7 +39,7 @@ function Calendar() {
               <h5 class="card-title">{event.name}</h5>
               <Link
                 to={{
-                  pathname: `/event/${event._id}`
+                  pathname: `/event/comments/${event._id}`
                 }}
               >
                 More Information
